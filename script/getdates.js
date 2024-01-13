102f1def45c352316beb666baef18039ccb66bcb
carolnacher <carolnacher@gmail.com>
2024-01-13 16:13:35 -0300

    first commit

---
+++ script/getdates.js
@@ -0,0 +1,13 @@
+document.addEventListener("DOMContentLoaded", function () {
+    let currentYear = new Date().getFullYear();
+    let firstParagraph = document.getElementById("footerFirstParagraph");
+
+    let decoratedName = "🌟 Carol Nacher 🌟";
+    let uruguayFlag = "Uruguay";
+    let uruguayFlagImage = `<img src="Images/flag.png" alt="Flag of Uruguay" width="95" height="75"style="vertical-align: middle;">`;
+
+    firstParagraph.innerHTML = `&copy; ${currentYear} ${decoratedName} -  ${uruguayFlag} ${uruguayFlagImage}`;
+
+    let lastModifiedParagraph = document.getElementById("lastModified");
+    lastModifiedParagraph.innerHTML = "Última modificación: " + document.lastModified;
+});
