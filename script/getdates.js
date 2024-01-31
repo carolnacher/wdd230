1bd1e49ba66862ac3a0788c0f4e9f308971aaa0e
carolnacher <carolnacher@gmail.com>
2024-01-31 17:19:44 -0300

    week04

--- script/getdates.js
+++ script/getdates.js
@@ -4,7 +4,7 @@ document.addEventListener("DOMContentLoaded", function () {
 
     let decoratedName = "🌟 Carol Nacher 🌟";
     let uruguayFlag = "Uruguay";
-    let uruguayFlagImage = `<img src="images/flag.png" alt="Flag of Uruguay" width="95" height="75" style="vertical-align: middle;">`;
+    let uruguayFlagImage = `<img src="images/flag.png" alt="Flag of Uruguay" width="200" height="200" style="vertical-align: middle;">`;
 
     firstParagraph.innerHTML = `&copy; ${currentYear} ${decoratedName} -  ${uruguayFlag} ${uruguayFlagImage}`;
 
