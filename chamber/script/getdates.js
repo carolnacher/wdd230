11be49553403a7ea7f8877fd9f7d6bb9f5738ab2
carolnacher <carolnacher@gmail.com>
2024-02-04 04:00:49 -0300

    week04

---
+++ chamber/script/getdates.js
@@ -0,0 +1,21 @@
+document.addEventListener("DOMContentLoaded", function () {
+    let currentYear = new Date().getFullYear();
+    let firstParagraph = document.getElementById("footerFirstParagraph");
+
+    let decoratedName = "🌟 Carol Nacher 🌟";
+    let uruguayFlag = "Uruguay";
+    let uruguayFlagImage = `<img src="images/flag.png" alt="Flag of Uruguay" width="200" height="200" style="vertical-align: middle;">`;
+
+    firstParagraph.innerHTML = `&copy; ${currentYear} ${decoratedName} -  ${uruguayFlag} ${uruguayFlagImage}`;
+
+    let lastModifiedParagraph = document.getElementById("lastModified");
+    lastModifiedParagraph.innerHTML = "Última modificación: " + document.lastModified;
+
+    const hamButton = document.querySelector('#menu');
+    const navigation = document.querySelector('.navigation');
+    
+    hamButton.addEventListener('click', () => {
+        navigation.classList.toggle('open');
+        hamButton.classList.toggle('open');
+    }); 
+});
