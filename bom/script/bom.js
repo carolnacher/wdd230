2161871bc3de7f0eb384d4ed73b53e4ccada40cf
carolnacher <carolnacher@gmail.com>
2024-02-23 11:58:34 -0300

    week07

--- bom/script/bom.js
+++ bom/script/bom.js
@@ -1,30 +1,58 @@
 const input = document.querySelector('#favchap');
 const button = document.querySelector('button');
-const list = document.querySelector('ul');
-const clearButton = document.querySelector('#clearList'); 
+const list = document.querySelector('#list');
+const clearButton = document.querySelector('#clearList');
+let chaptersArray = getChapterList() || [];
+
+clearButton.addEventListener('click', () => {
+  list.innerHTML = '';
+  chaptersArray = [];
+  setChapterList();
+  input.focus();
+});
+
+function displayList(item) {
+  let li = document.createElement('li');
+  let deleteButton = document.createElement('button');
+  li.textContent = item;
+  deleteButton.textContent = '❌';
+  deleteButton.classList.add('delete');
+  li.append(deleteButton);
+  list.append(li);
+  deleteButton.addEventListener('click', function () {
+    list.removeChild(li);
+    deleteChapter(li.textContent);
+    input.focus();
+  });
+}
+
+function setChapterList() {
+  localStorage.setItem('myFavBOMList', JSON.stringify(chaptersArray));
+}
+
+function getChapterList() {
+  const storedData = localStorage.getItem('myFavBOMList');
+  return storedData ? JSON.parse(storedData) : [];
+}
+
+function deleteChapter(chapter) {
+  chapter = chapter.slice(0, chapter.length - 1);
+  chaptersArray = chaptersArray.filter(item => item !== chapter);
+  setChapterList();
+}
 
 button.addEventListener('click', () => {
   if (input.value !== '') {
-
-    const li = document.createElement('li');
-    const deleteButton = document.createElement('button');
-    li.textContent = input.value;
-    deleteButton.textContent = '❌';
-    li.append(deleteButton);
-    list.append(li);
-    deleteButton.addEventListener('click', function () {
-      list.removeChild(li);
-      input.focus();
-    });
-
-    input.focus();
+    displayList(input.value);
+    chaptersArray.push(input.value);
+    setChapterList();
     input.value = '';
+    input.focus();
   } else {
- 
     input.focus();
   }
 });
-clearButton.addEventListener('click', () => {
-    list.innerHTML = '';
-    input.focus();
-  });
+
+chaptersArray.forEach(chapter => {
+  displayList(chapter);
+});
