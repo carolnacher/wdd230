48b00435ca1e7e0df54f249711e8a29fac39bc36
carolnacher <carolnacher@gmail.com>
2024-02-25 02:26:50 -0300

    week03

--- script/getdates.js
+++ script/getdates.js
@@ -3,7 +3,7 @@ document.addEventListener("DOMContentLoaded", function () {
     let firstParagraph = document.getElementById("footerFirstParagraph");
 
     let decoratedName = "🌟 Carol Nacher 🌟";
-    let uruguayFlag = "Uruguay";
+    let uruguayFlag = "WDD230 - Uruguay";
     let uruguayFlagImage = `<img src="images/flag.png" alt="Flag of Uruguay" width="200" height="200" style="vertical-align: middle;">`;
 
     firstParagraph.innerHTML = `&copy; ${currentYear} ${decoratedName} -  ${uruguayFlag} ${uruguayFlagImage}`;
