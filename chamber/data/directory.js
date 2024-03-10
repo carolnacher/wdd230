9850a04eac463f533788551bac19199464567dcf
carolnacher <carolnacher@gmail.com>
2024-03-10 00:35:51 -0300

    weel 09

--- chamber/data/directory.js
+++ chamber/data/directory.js
@@ -2,20 +2,20 @@ const baseURL = "https://carolnacher.github.io/wdd230";
 const linksURL = "https://carolnacher.github.io/wdd230/chamber/data/members.json";
 const cards = document.querySelector('#cards');
 const display = document.querySelector("article");
-const gridbutton = document.querySelector("#grid");
-const listbutton = document.querySelector("#list");
+const gridbutton = document.querySelector("#grida");
+const listbutton = document.querySelector("#lista");
 
 async function getMembers() {
   try {
     const response = await fetch(linksURL);
     const data = await response.json();
-    displayMembers(data.members); 
+    displayMembers(data.members);
   } catch (error) {
     console.error('Error fetching members data:', error);
   }
 }
 
-function displayMembers(members) { 
+function displayMembers(members) {
   const membersList = document.createElement('ul');
 
   members.forEach((member, index) => {
@@ -34,7 +34,6 @@ function displayMembers(members) {
       `Address: ${member.address}`,
       `Phone Number: ${member["phone number"]}`,
       `Website: ${member.url}`
-
     ];
 
     memberDetails.forEach((detail) => {
@@ -49,22 +48,20 @@ function displayMembers(members) {
     membersList.appendChild(memberItem);
   });
 
-  cards.innerHTML = ''; 
+  cards.innerHTML = '';
   cards.appendChild(membersList);
 }
 
 gridbutton.addEventListener("click", () => {
-	// example using arrow function
-	display.classList.add("grid");
-	display.classList.remove("list");
+  display.classList.add("grida");
+  display.classList.remove("lista");
 });
 
-listbutton.addEventListener("click", showList); // example using defined function
+listbutton.addEventListener("click", showList);
 
 function showList() {
-	display.classList.add("list");
-	display.classList.remove("grid");
+  display.classList.add("lista");
+  display.classList.remove("grida");
 }
-
 
 getMembers();
