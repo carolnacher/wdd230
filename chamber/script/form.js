33fd2afa785b4e24b781b5337274e1882410f465
carolnacher <carolnacher@gmail.com>
2024-03-11 23:49:21 -0300

    w09

--- chamber/script/form.js
+++ chamber/script/form.js
@@ -1,3 +1,23 @@
 var timestampField = document.getElementById("timestamp");
 var currentTimestamp = Date.now();
 timestampField.value = currentTimestamp;
+
+document.addEventListener("DOMContentLoaded", function () {
+	
+	function handleSubmit(event) {
+		
+		event.preventDefault();
+
+		
+		let formt = event.target;
+		let formData = new FormData(formt);
+
+		
+		for (let pair of formData.entries()) {
+			console.log(pair[0] + ": " + pair[1]);
+		}
+	}
+
+	const form = document.querySelector("form");
+	form.addEventListener("submit", handleSubmit);
+});
--- lesson09/script/prophets.js
+++ lesson09/script/prophets.js
@@ -11,14 +11,19 @@ getProphetData();
 
 const displayProphets = (prophets) => {
     prophets.forEach((prophet) => {
-        // Create elements to add to the div.cards element
+       
         let card = document.createElement('section');
         let fullName = document.createElement('h2');
+        let birthInfo = document.createElement('p'); 
         let portrait = document.createElement('img');
 
-        // Build the h2 content out to show the prophet's full name
+        
         fullName.textContent = `${prophet.name} ${prophet.lastname}`;
-        // Build the image portrait by setting all the relevant attributes
+
+       
+        birthInfo.innerHTML = `<strong>Birth date:</strong> ${prophet.birthdate}<br><strong>Place Birth:</strong> ${prophet.birthplace}`;
+
+       
         portrait.setAttribute('src', prophet.imageurl);
         portrait.setAttribute('alt', `Portrait of ${prophet.name} ${prophet.lastname}`);
         portrait.setAttribute('loading', 'lazy');
@@ -26,6 +31,7 @@ const displayProphets = (prophets) => {
         portrait.setAttribute('height', '440');
 
         card.appendChild(fullName);
+        card.appendChild(birthInfo); 
         card.appendChild(portrait);
 
         cards.appendChild(card);
