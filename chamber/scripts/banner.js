e043ecbd640577e6fa131fb0dc854ba3d6234633
carolnacher <carolnacher@gmail.com>
2024-03-16 01:54:44 -0300

    week10

--- chamber/scripts/banner.js
+++ chamber/scripts/banner.js
@@ -5,7 +5,7 @@ document.addEventListener("DOMContentLoaded", function() {
     var today = new Date();
     var dayOfWeek = today.getDay();
   
-    if (dayOfWeek >= 1 && dayOfWeek <= 5) {
+    if (dayOfWeek >= 1 && dayOfWeek <= 3) {
       banner.style.display = "block";
     }
   
