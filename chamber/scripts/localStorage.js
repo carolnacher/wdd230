a5415682c9a473a174af2010e6aeacb077f8c500
carolnacher <carolnacher@gmail.com>
2024-03-21 00:55:49 -0300

    WEEK11

--- chamber/scripts/localStorage.js
+++ chamber/scripts/localStorage.js
@@ -1,70 +1,61 @@
 document.addEventListener("DOMContentLoaded", function () {
-    var currentDate = new Date();
-    var lastVisitDate = localStorage.getItem("lastVisitDate");
-    var visitCount = localStorage.getItem("visitCount");
-  
-    if (!lastVisitDate) {
+  var currentDate = new Date();
+  var lastVisitDate = localStorage.getItem("lastVisitDate");
+
+  if (!lastVisitDate) {
       displayWelcomeMessage();
-    } else {
+  } else {
       currentDate = new Date();
       lastVisitDate = new Date(lastVisitDate);
-  
+
       var timeDiff = currentDate - lastVisitDate;
       var daysDiff = Math.floor(timeDiff / (1000 * 60 * 60 * 24));
-  
+
       if (daysDiff < 1) {
-        displaySoonMessage();
+          displaySoonMessage();
       } else {
-        displayLastVisitMessage(daysDiff);
+          displayLastVisitMessage(daysDiff);
       }
-    }
-  
-    updateVisitCount();
-    updateVisitorInfo()
-  
-    localStorage.setItem("lastVisitDate", currentDate);
-  });
-  
-  function displayWelcomeMessage() {
-    updateVisitorInfo("Welcome! Let us know if you have any questions.");
   }
-  
-  function displaySoonMessage() {
-    updateVisitorInfo("Back so soon! Awesome!");
-  }
-  
-  function displayLastVisitMessage(daysDiff) {
-    var message = "You last visited " + daysDiff + " day" + (daysDiff !== 1 ? "s" : "") + " ago";
-  
-    
-    var visitMessageElement = document.getElementById("visitMessage");
-  
-    
-    visitMessageElement.textContent = message;
-  }
-  
-  
-  var daysDifference = 3; 
-  displayLastVisitMessage(daysDifference);
-  
-  function updateVisitCount() {
-    var visitCount = localStorage.getItem("visitCount") || 0;
-    visitCount++;
-    localStorage.setItem("visitCount", visitCount);
-  }
-  
-  function updateVisitorInfo(message) {
-    var visitCount = localStorage.getItem("visitCount") || 0;
-    var todayElement = document.getElementById("today");
-    var visitsElement = document.querySelector(".visits");
-    var currentDate = new Date().toLocaleDateString();
-  
-    todayElement.textContent = currentDate;
-    visitsElement.textContent = visitCount;
-  
-    
-    var messageElement = document.createElement("p");
-    messageElement.textContent = message;
-    document.getElementById("visitor").appendChild(messageElement);
-  }
-  
+
+  updateVisitCount();
+  updateVisitorInfo()
+
+  localStorage.setItem("lastVisitDate", currentDate);
+});
+
+function displayWelcomeMessage() {
+  updateVisitorInfo("Welcome! Let us know if you have any questions.");
+}
+
+function displaySoonMessage() {
+  updateVisitorInfo("Back so soon! Awesome!");
+}
+
+function displayLastVisitMessage(daysDiff) {
+  var message = "You last visited " + daysDiff + " day" + (daysDiff !== 1 ? "s" : "") + " ago";
+
+  var visitMessageElement = document.getElementById("visitMessage");
+
+  visitMessageElement.textContent = message;
+}
+
+function updateVisitCount() {
+  var visitCount = localStorage.getItem("visitCount") || 0;
+  visitCount++;
+  localStorage.setItem("visitCount", visitCount);
+}
+
+function updateVisitorInfo(message) {
+  var visitCount = localStorage.getItem("visitCount") || 0;
+  var todayElement = document.getElementById("today");
+  var visitsElement = document.querySelector(".visits");
+  var currentDate = new Date().toLocaleDateString();
+
+  todayElement.textContent = currentDate;
+  visitsElement.textContent = visitCount;
+
+  var messageElement = document.createElement("p");
+  messageElement.textContent = message;
+  document.getElementById("visitor").appendChild(messageElement);
+}
