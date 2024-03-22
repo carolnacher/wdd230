9b0da68def24a3361e71461c262f66977686c727
carolnacher <carolnacher@gmail.com>
2024-03-22 00:27:11 -0300

    week11

--- chamber/scripts/getdates.js
+++ chamber/scripts/getdates.js
@@ -2,18 +2,18 @@ document.addEventListener("DOMContentLoaded", function () {
     let currentYear = new Date().getFullYear();
     let firstParagraph = document.getElementById("footerFirstParagraph");
 
-    let decoratedName = "🌟 Carol Nacher 🌟";
-    let uruguayFlag = "WDD230 - Uruguay";
-    let uruguayFlagImage = `<img src="images/flag.webp" alt="Flag of Uruguay" width="200" height="200" style="vertical-align: middle;">`;
+    let decoratedName = "Contact Chamber Montevideo";
+    let uruguayFlag = "Rincón 454 - 598 2916 1277 - 598 2916 1243";
+    let uruguayFlagImage = `<img src="images/flag.webp" alt="Flag of Uruguay" width="80" height="80" style="vertical-align: middle;">`;
 
-    firstParagraph.innerHTML = `&copy; ${currentYear} ${decoratedName} -  ${uruguayFlag} ${uruguayFlagImage}`;
+    firstParagraph.innerHTML = ` ${decoratedName} -  ${uruguayFlag} ${uruguayFlagImage} - ${currentYear}&copy;`;
 
     let lastModifiedParagraph = document.getElementById("lastModified");
     lastModifiedParagraph.innerHTML = "Última modificación: " + document.lastModified;
     var timestampField = document.getElementById('timestamp');
 
 
-     
+    
 });
 
 
